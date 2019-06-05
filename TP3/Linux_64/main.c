8108af7afbb9c727af8be0e86b4fde72045e6bcd
alumno <alumno@LAB00PC00>
2019-06-04 22:03:53 -0300

    tp3

---
+++ TP3/Linux_64/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+#include "parser.h"
+
+/****************************************************
+    Menu:
+     1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).
+     2. Cargar los datos de los empleados desde el archivo data.csv (modo binario).
+     3. Alta de empleado
+     4. Modificar datos de empleado
+     5. Baja de empleado
+     6. Listar empleados
+     7. Ordenar empleados
+     8. Guardar los datos de los empleados en el archivo data.csv (modo texto).
+     9. Guardar los datos de los empleados en el archivo data.csv (modo binario).
+    10. Salir
+*****************************************************/
+
+
+int main()
+{
+    FILE* pFILE=fopen("data.csv","r");
+    Employee* pAux;
+    char nombre[30];
+   // int option = 0;
+    LinkedList* listaEmpleados = ll_newLinkedList();
+    /*do{
+        switch(option)
+        {
+            case 1:
+                controller_loadFromText("data.csv",listaEmpleados);
+                break;
+        }
+    }while(option != 10);*/
+    parser_EmployeeFromText(pFILE,listaEmpleados);
+    for(int i =0;i<ll_len(listaEmpleados);i++)
+    {
+        pAux=ll_get(listaEmpleados,i);
+        employee_getNombre(pAux,nombre);
+        printf("%s\n",nombre);
+
+
+    }
+    return 0;
+}
---
+++ TP3/Linux_64/parser.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+    int retorno=-1;
+    int cantidad;
+    char bufferId[4096];
+    char bufferNombre[4096];
+    char bufferHorasTrabajadas[4096];
+    char bufferSueldo[4096];
+    Employee* pEmployee;
+
+    if(pFile !=NULL && pArrayListEmployee != NULL)
+    {
+        //pFile = fopen("data.csv","r");
+        do
+        {
+
+            cantidad=fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n",bufferId,bufferNombre,bufferHorasTrabajadas,bufferSueldo);
+            if(cantidad==4)
+            {
+                pEmployee=employee_newParametros(bufferId,bufferNombre,bufferHorasTrabajadas,bufferSueldo);
+                if(pEmployee !=NULL)
+                {
+                    ll_add(pArrayListEmployee,pEmployee);
+                    retorno = 1;
+                }
+            }
+        }while(!feof(pFile));
+
+        fclose(pFile);
+    }
+
+    return retorno;
+}
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+
+    return 1;
+}
