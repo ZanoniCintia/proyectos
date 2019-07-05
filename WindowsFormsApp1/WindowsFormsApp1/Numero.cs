da7550a88f705ef6ec0d89a1ae03858d7ed8e873
alumno <alumno>
2019-07-04 22:07:15 -0300

    calculadora c#

---
+++ WindowsFormsApp1/WindowsFormsApp1/Numero.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Numero
+    {
+        private double numero;
+
+        public void setNumero(double numero)
+        {
+
+            this.numero = numero;
+
+        }
+        public double GetNumero()
+        {
+            return this.numero;
+        }
+
+        private static double ValidarNumero(string numero)
+        {
+            double retorno;
+            if (!double.TryParse(numero, out retorno))
+            {
+                retorno = 0;
+            }
+            return retorno;
+        }
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+        public Numero(string numero)
+        {
+            this.numero = Numero.ValidarNumero(numero);
+        }
+        public static double SumarNumeros(Numero n1,Numero n2)
+        {
+            double retorno;
+            retorno = n1.GetNumero() + n2.GetNumero();
+            return retorno;
+        }
+    }
+}
